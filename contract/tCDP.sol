38bdf6a4d8df0b58b202533ad6677a0810140365
artistic709 <artistic709@gmail.com>
2020-05-13 23:05:24 +0800

    forget to pay dai, so dumb

--- contract/tCDP.sol
+++ contract/tCDP.sol
@@ -241,6 +241,7 @@ contract tCDP is ERC20Mintable {
 
         _burn(msg.sender, amount);
 
+        Dai.transferFrom(msg.sender, address(this), amount);
         cDai.repayBorrow(tokenToRepay);
         cEth.redeemUnderlying(tokenToDraw);
         (bool success, ) = msg.sender.call.value(tokenToDraw)("");
