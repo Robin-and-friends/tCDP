e82f012ed9e49a04ca28a7f9e6d585f3d516c246
robinpan1 <robinpan2503@gmail.com>
2020-05-22 00:13:53 +0800

    add unit test - compound mint

--- test/tCDP.test.js
+++ test/tCDP.test.js
@@ -24,15 +24,20 @@
   
     let collateral = 0;
     let debt = 0;
+    const maxPossibleInterest = ether('0.0001');
 
+    before(async function() {
+      this.dai = await ERC20.at(addr.DAI.TOKEN_ADDRESS);
+    });
+    
     beforeEach(async function() {
       this.tcdp = await tCDP.new();
     });
   
     describe("Compound", function() {
       const isCompound = true;
-      let supplyAmt = ether('80');
-      let borrowAmt = ether('1');
+      let supplyAmt = ether('1');
+      let borrowAmt = ether('80');
 
       it('deploy', async function() {
         await this.tcdp.setIsCompound(isCompound); 
@@ -42,12 +47,45 @@
   
       it('initiate', async function() {
         await this.tcdp.initiate(borrowAmt, {value: supplyAmt, from: user});
-
         [collateral, debt] = await getCollateralAndDebt(this.tcdp);
+        let tcdpBalance = await this.tcdp.balanceOf.call(user);
 
-        console.log(`collateral: ${collateral}`);
-        console.log(`debt: ${debt}`);
-  
+        expect(collateral).to.be.bignumber.eq(supplyAmt);
+        expect(debt).to.be.bignumber.eq(borrowAmt);
+        expect(tcdpBalance).to.be.bignumber.eq(supplyAmt);
+      });
+
+      it('mint', async function() {
+        let mintAmt = ether('1');
+        [collateralBefore, debtBefore] = await getCollateralAndDebt(this.tcdp);
+        const totalSupply = await this.tcdp.totalSupply.call();
+        const tcdpBalanceBefore = await this.tcdp.balanceOf.call(user);
+        const daiBalanceBefore = await this.dai.balanceOf.call(user);
+
+        const tokenToMint = mintAmt
+          .mul(new BN(totalSupply))
+          .div(new BN(collateralBefore));
+        const tokenToBorrow = mintAmt
+          .mul(new BN(debtBefore))
+          .div(new BN(collateralBefore));
+
+        await this.tcdp.mint({value: mintAmt, from: user});
+
+        [collateralAfter, debtAfter] = await getCollateralAndDebt(this.tcdp);
+        const totalSupplyAfter = await this.tcdp.totalSupply.call();
+        const tcdpBalanceAfter = await this.tcdp.balanceOf.call(user);
+        const daiBalanceAfter = await this.dai.balanceOf.call(user);
+
+        expect(collateralAfter).to.be.bignumber
+          .sub(supplyAmt.add(mintAmt))
+          .within(new BN(0), maxPossibleInterest);
+        expect(debtAfter).to.be.bignumber
+          .sub(borrowAmt.add(new BN(tokenToBorrow)))
+          .within(new BN(0), maxPossibleInterest);
+        expect(tcdpBalanceAfter).to.be.bignumber
+          .eq(tcdpBalanceBefore.add(tokenToMint));
+        expect(daiBalanceAfter).to.be.bignumber
+          .eq(daiBalanceBefore.add(tokenToBorrow));
       });
   
   
