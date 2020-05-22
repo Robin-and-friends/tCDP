a4f7407b564d4fdad2ef92c9f1b169d3ecddb01d
robinpan1 <robinpan2503@gmail.com>
2020-05-22 22:12:40 +0800

    add unlock accounts

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,7 +26,7 @@ if ganache_running 8545; then
 else
   echo "Starting our own ganache instance"
 
-  npx ganache-cli -p 8545 -f https://mainnet.infura.io/v3/$INFURA_API_KEY -m "$TEST_MNEMONIC_PHRASE" > ganache.log & ganache_pid=$!
+  npx ganache-cli -p 8545 -f https://mainnet.infura.io/v3/$INFURA_API_KEY -m "$TEST_MNEMONIC_PHRASE" -u "0x742d35Cc6634C0532925a3b844Bc454e4438f44e" -u "0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667" -u "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"> ganache.log & ganache_pid=$!
 fi
 
 # Run the truffle test or the solidity-coverage suite.
